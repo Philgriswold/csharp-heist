efdb6d5d23e183b8d41d539a0bf22f03b3ab975c
Phil Griswold <philgriswold@me.com>
2019-11-18 09:49:02 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan your Heist!");
+
+            Dictionary<string, string> teamMember = new Dictionary<string, string>();
+            string name = "Bill";
+            string skill = "9";
+            string courage = "1.4";
+
+            Console.WriteLine("What is your name?");
+            string newName = Console.ReadLine();
+        }
+    }
+}
